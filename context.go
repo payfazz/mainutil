149a31dee807f345c2938c6646440258533fc80b
Kurnia D Win <kurnia.d.win@gmail.com>
2020-06-06 17:05:11 +0700

    update

--- context.go
+++ context.go
@@ -5,25 +5,41 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"sync"
 
 	"github.com/payfazz/stdlog"
 )
 
-// WaitForInterruptThenCancelContext .
-func WaitForInterruptThenCancelContext(ctx context.Context, cancel context.CancelFunc) {
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, getInterruptSigs()...)
-
-	var sig os.Signal
-	select {
-	case sig = <-c:
-	case <-ctx.Done():
+var (
+	backgroundContext struct {
+		once   sync.Once
+		ctx    context.Context
+		cancel context.CancelFunc
 	}
+)
 
-	signal.Stop(c)
-	if sig != nil {
-		stdlog.PrintOut(fmt.Sprintf("Got signal %s\n", sig.String()))
-	}
+// BackgroundContext return context that will be done when the program interrupted (SIGINT, SIGTERM)
+func BackgroundContext() (context.Context, context.CancelFunc) {
+	backgroundContext.once.Do(func() {
+		backgroundContext.ctx, backgroundContext.cancel = context.WithCancel(context.Background())
 
-	cancel()
+		go func() {
+			c := make(chan os.Signal, 1)
+			signal.Notify(c, getInterruptSigs()...)
+
+			var sig os.Signal
+			select {
+			case sig = <-c:
+			case <-backgroundContext.ctx.Done():
+			}
+
+			signal.Stop(c)
+			if sig != nil {
+				stdlog.PrintOut(fmt.Sprintf("Got signal %s", sig.String()))
+			}
+
+			backgroundContext.cancel()
+		}()
+	})
+	return backgroundContext.ctx, backgroundContext.cancel
 }
